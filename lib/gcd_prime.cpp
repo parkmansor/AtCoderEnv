ff36682d132a91ae198baec498eb03f9906bee10
parkmansor <57488489+parkmansor@users.noreply.github.com>
2020-08-31 22:09:03 +0900

    Update gcd_prime.cpp

--- lib/gcd_prime.cpp
+++ lib/gcd_prime.cpp
@@ -57,3 +57,26 @@ vector<ll> divisorExec(ll n)
 	sort(res.begin(), res.end());
 	return res;
 }
+
+/* 素数の列挙 */
+std::vector<int> Eratosthenes( const int N )
+{
+    std::vector<bool> is_prime( N + 1 );
+    for( int i = 0; i <= N; i++ )
+    {
+        is_prime[ i ] = true;
+    }
+    std::vector<int> P;
+    for( int i = 2; i <= N; i++ )
+    {
+        if( is_prime[ i ] )
+        {
+            for( int j = 2 * i; j <= N; j += i )
+            {
+                is_prime[ j ] = false;
+            }
+            P.emplace_back( i );
+        }
+    }
+    return P;
+}
