b0ab31279dc65cf7837c183f066c72476df3d0af
parkmansor <57488489+parkmansor@users.noreply.github.com>
2020-08-31 22:11:16 +0900

    Update gcd_prime.cpp

--- lib/gcd_prime.cpp
+++ lib/gcd_prime.cpp
@@ -59,24 +59,21 @@ vector<ll> divisorExec(ll n)
 }
 
 /* 素数の列挙 */
-std::vector<int> Eratosthenes( const int N )
+vector<int> Eratosthenes(int N)
 {
-    std::vector<bool> is_prime( N + 1 );
-    for( int i = 0; i <= N; i++ )
-    {
-        is_prime[ i ] = true;
+    std::vector<bool> is_prime(N + 1);
+    for(int i = 0; i <= N; i++) {
+        is_prime[i] = true;
     }
-    std::vector<int> P;
-    for( int i = 2; i <= N; i++ )
-    {
-        if( is_prime[ i ] )
-        {
-            for( int j = 2 * i; j <= N; j += i )
-            {
-                is_prime[ j ] = false;
+
+    std::vector<int> res;
+    for(int i = 2; i <= N; i++ ) {
+        if (is_prime[i]) {
+            for(int j = 2 * i; j <= N; j += i) {
+                is_prime[j] = false;
             }
-            P.emplace_back( i );
+            res.emplace_back(i);
         }
     }
-    return P;
+    return res;
 }
