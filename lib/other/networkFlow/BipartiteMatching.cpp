4b47b1b835d80d4d95ef4c70ac18f133e5650f0e
parkmansor <57488489+parkmansor@users.noreply.github.com>
2020-10-07 22:40:54 +0900

    Update BipartiteMatching.cpp

--- lib/other/networkFlow/BipartiteMatching.cpp
+++ lib/other/networkFlow/BipartiteMatching.cpp
@@ -1,46 +1,3 @@
-#include <iostream>
-#include <set>
-#include <queue>
-#include <vector>
-#include <algorithm>
-#include <math.h>
-#include <cmath>
-#include <string>
-#include <cstring>
-#include <climits>
-#include <sstream>
-#include <iomanip>
-#include <map>
-#include <stack>
-#include <tuple>
-#include <numeric>
-#include <functional>
-#include <unordered_map>
-
-using namespace std;
-
-/*-----------------------------------------------------------------------------
-　定義
- -------------------------------------------------------------------------------*/
-#define REP(i, n)				for (int (i) = 0 ; (i) < (int)(n) ; ++(i))
-#define REPN(i, m, n)			for (int (i) = m ; (i) < (int)(n) ; ++(i))
-#define INF						2e9
-#define MOD						(1000 * 1000 * 1000 + 7)
-#define Ceil(x, n)				(((((x))+((n)-1))/n))		/* Nの倍数に切り上げ割り算 */
-#define CeilN(x, n)				(((((x))+((n)-1))/n)*n)		/* Nの倍数に切り上げ */
-#define FloorN(x, n)			((x)-(x)%(n))				/* Nの倍数に切り下げ */
-#define IsOdd(x)				(((x)&0x01UL) == 0x01UL)			
-#define IsEven(x)				(!IsOdd((x)))						
-#define ArrayLength(x)			(sizeof( x ) / sizeof( x[ 0 ]))
-#define	MAX_QWORD				((QWORD)0xFFFFFFFFFFFFFFFF)
-#define M_PI					3.14159265358979323846
-typedef long long				ll;
-typedef unsigned long long int	QWORD;
-typedef pair<ll, ll>			P;
-
-/*-----------------------------------------------------------------------------
-　処理
- -------------------------------------------------------------------------------*/
 // 二部マッチング
 static const int pairNothing = -1;
 class BipartiteMatching
@@ -170,6 +127,5 @@ int main()
 	// 全員の分銅合計 － 切ったペア両方が使える数　
 	int ans = res.V - res.GetMatchingPairNum();
 	cout << ans << endl;
-	return 0;
 }
 
