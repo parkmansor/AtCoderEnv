bb8dc44efacd8302e7da6597726c967d7f50cbd8
parkmansor <57488489+parkmansor@users.noreply.github.com>
2020-11-03 19:20:22 +0900

    Update myMathLib.cpp

--- lib/myMathLib.cpp
+++ lib/myMathLib.cpp
@@ -123,3 +123,18 @@ bool masuCheck(int sx, int sy)
 
 	return true;
 }
+
+// Combination Table
+ll C[51][51]; // C[n][k] -> nCk
+void comb_table(int N){
+	for (int i = 0; i <= N; ++i){
+		for (int j = 0; j <= i; ++j){
+			if (j == 0 || j == i){
+				C[i][j] = 1LL;
+			} else{
+				C[i][j] = (C[i - 1][j - 1] + C[i - 1][j]);
+			}
+		}
+	}
+}
+
